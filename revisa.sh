12d570d70837d3cae3b597260bb50b960ec0e441
Jose Lopez <jlopez@frontend.cluster.com>
2017-02-26 17:03:06 -0500

    runall lanza simulaciones de 4 en 4

--- revisa.sh
+++ revisa.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+cuenta=0
 for i in *.lst.bz2
 do
 	bzcat $i | tail -1 | awk '$0!~/^ ========== END OF RUN ================================================$/{print "'$i':KO"}'
+	let cuenta+=1
 	done
+echo "Hay $cuenta archivos .lst.bz2"
--- runall.sh
+++ runall.sh
@@ -3,8 +3,13 @@ if [ -z "$1" ]
 then
 	echo -e "USAGE ./runall.sh ProjectName \n\n(ProjectName from ./do_halley.sh)"
 else
-	for i in go-$1-*
+	cuantos=0
+	for i in go-$1-*.sh
 	do
-		./$i
+		if [ "$cuantos" -lt "4" ]
+		then
+			./$i
+			let cuantos+=1
+		fi
 		done
 	fi
