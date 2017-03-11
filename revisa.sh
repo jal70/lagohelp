7cd4a5cf6b79946156926f9dde774c1a042b669e
Jose Lopez <jlopez@frontend.cluster.com>
2017-03-11 10:04:21 -0500

    Revisa los archivos secundarios

--- revisa.sh
+++ revisa.sh
@@ -5,4 +5,12 @@ do
 	bzcat $i | tail -1 | awk '$0!~/^ ========== END OF RUN ================================================$/{print "'$i':KO"}'
 	let cuenta+=1
 	done
-echo "Hay $cuenta archivos .lst.bz2"
+echo "Hay $cuenta archivos tipo .lst.bz2"
+#######
+cuenta=0
+for i in *.sec.bz2
+do
+	bzcat $i | tail -1 | awk '$0!~/^\# \# [0-9]+ lines read on [0-9]+ secondaries for [0-9]+ showers$/{print "'$i':KO"}'
+	let cuenta+=1
+	done
+echo "Hay $cuenta archivos tipo .sec.bz2"
