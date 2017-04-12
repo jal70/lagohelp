77535ac5e356323bf817f06405dcd98e6ff7f783
jose <jal.ccs.sn@gmail.com>
2017-04-12 19:58:36 -0400

    opciones descomprimir

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+NUMBEROFFILES=1
+while [ "$1" != "" ]
+do
+	case $1 in
+	-n)
+	NUMBEROFFILES=$2
+	;;
+	-h)
+	echo "USO: $nohup ./descomprimir.sh -n numero_de_archivos_a_descomprimir &"
+	exit 0
+	esac
+	# Shift all the parameters down by one
+	shift
+done
 if [ ! -e salida/procesados_descomprimir.dat ]
 then
 	echo "Ejecute preparaextraccion.sh"
@@ -9,7 +23,7 @@ for i in DAT??????.bz2
 do
 	procesado=$(awk 'BEGIN{salida=1}{if($0=="'$i'"){salida=0}}END{print salida}' salida/procesados_descomprimir.dat)
 	echo $i $procesado "(0 -> fue procesado, 1 -> no ha sido procesado)"
-	if [ "$cuenta" -lt "3" ] && [ "$procesado" == "1" ]
+	if [ "$cuenta" -lt "$NUMBEROFFILES" ] && [ "$procesado" == "1" ]
 	then
 		j=$(echo $i | sed -e 's/.bz2//')
 		u=$(echo $j | sed -e 's/DAT//')
